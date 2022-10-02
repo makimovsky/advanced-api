6d68283b5296fe375b470cd7ffc5c73d675dd17c
makimovsky <makimofficial@gmail.com>
2022-10-02 14:33:00 +0200

    Set up the django admin.

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -18,20 +18,20 @@ class AdminSiteTests(TestCase):
             password='testpass123',
             name='Test User'
         )
-    
+
     def test_users_list(self):
         url = reverse('admin:core_user_changelist')
         res = self.client.get(url)
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
-    
+
     def test_edit_user_page(self):
         url = reverse('admin:core_user_change', args=[self.user.id])
         res = self.client.get(url)
 
         self.assertEqual(res.status_code, 200)
-    
+
     def test_create_user_page(self):
         url = reverse('admin:core_user_add')
         res = self.client.get(url)
